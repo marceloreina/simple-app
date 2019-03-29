89e7c4246ad97b000c0ec75290ee525e017d04ba
Marcelo Reina <marceloreina@gmail.com>
2019-03-29 09:58:11 +0000

    Added single view project

---
+++ SimpleApp/SimpleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleApp
+//
+//  Created by Marcelo Reina on 29/03/2019.
+//  Copyright © 2019 Marcelo Reina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
